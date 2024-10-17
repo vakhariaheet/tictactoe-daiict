044f8d714904620c84ae62d061ecc2ddf6692964
Heet Vakharia <heetkv@gmail.com>
2024-10-17 19:53:21 +0530

    Updated Gemini API and Optimise Prompts

--- client/src/pages/SignIn.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from '@clerk/clerk-react'
-
-export default function SignInPage() {
-  return <div className="h-screen flex justify-center items-center">
-    <SignIn path="/sign-in" signUpUrl="/sign-up" />
-    </div>
-}
